31d732746816bc65ca70111b1b4ce0c4b185ac8a
sfstacey <stacey.jenkins@screamingfrog.co.uk>
2016-06-16 17:30:11 +0100

    BACKUP

--- js/app/main.js
+++ js/app/main.js
@@ -5,6 +5,7 @@ require(['jquery'], function(){
   $(document).ready(function(){
 
       var $imgpath = 'img/svg/';
+      var $moneybutton = $('a.button');
       var $coinpilelink = $('.coinpile a');
       var $landmarks = {
       landmark:
@@ -61,11 +62,16 @@ require(['jquery'], function(){
       });
 
       //tooltips
+
+      $coinpilelink.on('click', function(){
+        return false;
+      })
+
       $coinpilelink.on('mouseover', function(){
      var $section_position = parseInt($(this).parent().index());
     $('.tooltip').find('p.landmark_name').html($landmarks.landmark[$section_position].name);
     $('.tooltip').find('p.landmark_cost').html($landmarks.landmark[$section_position].cost);
-     $('.tooltip').show();
+     $('.tooltip').fadeIn(100);
 
       }).mousemove(function(ev){
       var mousex = ev.pageX - 330;
@@ -76,11 +82,22 @@ require(['jquery'], function(){
       $coinpilelink.on('mouseleave', hideTooltip);
 
       function hideTooltip(e){
-      $('.tooltip').hide();
+      $('.tooltip').fadeOut(200);
       }
 
+      $moneybutton.on('click', startSite);
+
+      function startSite(){
+        console.log('click');
+        $('.title_wrapper').fadeOut(300);
+        return false;
+      };
 
       preloadArtwork($imgpath);
+
+      $(window).load(function() {
+      $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+}     );
 
   });
 })(jQuery);
@@ -88,7 +105,6 @@ require(['jquery'], function(){
 
 function preloadArtwork($imgpath){
 $('.pointer').load($imgpath+'pointer.svg');
-$('.rio_clouds').load($imgpath+'rio_clouds.svg');
 $('section#rio .redeemer').load($imgpath+'rio_redeemer.svg');
 $('section#rio .rio_trees').load($imgpath+'rio_trees.svg');
 $('section#london .supports').load($imgpath+'london_supports.svg');
@@ -103,11 +119,15 @@ $('section#flight .plane').load($imgpath+'plane.svg');
 $('.cloud_left').load($imgpath+'cloud_left.svg');
 $('.cloud_right').load($imgpath+'cloud_right.svg');
 $('.texture').load($imgpath+'texture.svg');
-$('.logo_graphic').load($imgpath+'logo.svg');
 
 };
 
 function loadAnimations(ScrollMagic, $animation_controller){
+
+//skyparallax
+var cloudlayer2 = new ScrollMagic.Scene({triggerElement:'.foreground_clouds', offset:650, duration:4500}).setTween(TweenMax.to(".foreground_clouds",1, {'bottom':'10%'})).addTo($animation_controller);
+var cloudlayer1 = new ScrollMagic.Scene({triggerElement:'.background_clouds', offset:650, duration:8000}).setTween(TweenMax.to(".background_clouds",1, {'bottom':'-20%'})).addTo($animation_controller);
+
 //skycolours
 var space_sky = new ScrollMagic.Scene({triggerElement:'#space', duration:960}).setTween(TweenMax.to("body",1, {'background-color':'rgb(7,2,49)'})).addTo($animation_controller);
 
@@ -126,13 +146,13 @@ var airplane = new ScrollMagic.Scene({triggerElement:'#flight', duration:1300}).
 
 
 //londoneye
-var london_eye_spin = new ScrollMagic.Scene({triggerElement:'#london', offset:-500,  duration:1850}).setTween(TweenMax.to(".london_eye",1, {rotation:360})).addTo($animation_controller);
+var london_eye_spin = new ScrollMagic.Scene({triggerElement:'#london', offset:-500,  duration:1850}).setTween(TweenMax.to(".london_eye",1, {rotation:100})).addTo($animation_controller);
 
 //coinpiles
 var flight_coinpile = new ScrollMagic.Scene({triggerElement:'#flight', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.flight",1,{'display':'none', 'height':'0'})).addTo($animation_controller);
 var nepal_coinpile = new ScrollMagic.Scene({triggerElement:'#nepal', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.nepal",1,{'display':'none', 'height':'0'})).addTo($animation_controller);
-var capetown_coinpile = new ScrollMagic.Scene({triggerElement:'#capetown', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.capetown",1,{'display':'none', 'height':'0'})).addTo($animation_controller).addIndicators();
-var dubai_coinpile = new ScrollMagic.Scene({triggerElement:'#dubai', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.dubai",1,{'display':'none', 'height':'0'})).addTo($animation_controller).addIndicators();
+var capetown_coinpile = new ScrollMagic.Scene({triggerElement:'#capetown', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.capetown",1,{'display':'none', 'height':'0'})).addTo($animation_controller)
+var dubai_coinpile = new ScrollMagic.Scene({triggerElement:'#dubai', offset:410, duration:3000}).setTween(TweenMax.to(".coinpile.dubai",1,{'display':'none', 'height':'0'})).addTo($animation_controller)
 var newyork_link = new ScrollMagic.Scene({triggerElement:'#newyork', offset:660}).setTween(TweenMax.to(".coinpile.newyork a",1,{'display':'none'})).addTo($animation_controller);
 var newyork_coinpile = new ScrollMagic.Scene({triggerElement:'#newyork', offset:110, duration:2000}).setTween(TweenMax.to(".coinpile.newyork",1,{'display':'none', 'height':'0'})).addTo($animation_controller);
 var paris_link = new ScrollMagic.Scene({triggerElement:'#paris', offset:660}).setTween(TweenMax.to(".coinpile.paris a",1,{'display':'none'})).addTo($animation_controller);
